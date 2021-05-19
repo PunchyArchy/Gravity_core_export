ea4bdd9bda17dc1bb519c7420858cdc4b97a47bb
PunchyArchy <ksmdrmvstchny@gmail.com>
2021-05-19 10:39:22 +0500

    Внедрен метод на изменение записи

--- gravity_core/api/service_functions.py
+++ gravity_core/api/service_functions.py
@@ -3,6 +3,7 @@ from traceback import format_exc
 
 
 def execute_api_method(core_method, *args, **kwargs):
+    """ Выполнить метод core_method ядра core, передать ему аргументы """
     try:
         threading.Thread(target=core_method, args=args, kwargs=kwargs).start()
         response = {'status': 'success', 'info': 'Протокол заезда успешно начат'}
